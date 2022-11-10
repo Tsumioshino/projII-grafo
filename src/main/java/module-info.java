ae975f0b0e4ba5105b1deb0cb9c0f0dcf2d43a72
Alfredo Gabriel <amgabriel1@hotmail.com>
2022-11-10 11:34:32 -0300

    update: add module and javafx to project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.projII_grafo {
+    requires java.sql;
+    requires java.desktop;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+    
+    exports com.projII_grafo;
+
+}

92d9726347bade43c42ba86844293f37966f4fe6
Tsumioshino <marcus.oliveira@icen.ufpa.br>
2022-12-04 21:52:44 -0300

    fix: double change dependencies bug

--- src/main/java/com/projII_grafo/StrategyAdjMatrix.java
+++ src/main/java/com/projII_grafo/StrategyAdjMatrix.java
@@ -9,10 +9,10 @@ import java.util.ArrayList;
 public class StrategyAdjMatrix implements StrategyStructure {
 
   ArrayList<String> order;
-  ArrayList<ArrayList<Integer>> matrix;
+  ArrayList<ArrayList<Double>> matrix;
 
   public StrategyAdjMatrix() {
-    this.matrix = new ArrayList<ArrayList<Integer>>();
+    this.matrix = new ArrayList<ArrayList<Double>>();
     this.order = new ArrayList<String>();
   }
 
@@ -53,9 +53,9 @@ public class StrategyAdjMatrix implements StrategyStructure {
 
 
   @Override
-  public int getArestaValue(String origem, String destino) {
-    int origem_ind = this.order.indexOf(origem);
-    int destino_ind = this.order.indexOf(destino);
+  public double getArestaValue(String v_origem, String v_destino) {
+    int origem_ind = this.order.indexOf(v_origem);
+    int destino_ind = this.order.indexOf(v_destino);
     return this.matrix.get(origem_ind).get(destino_ind);
   }
   
@@ -63,14 +63,14 @@ public class StrategyAdjMatrix implements StrategyStructure {
   public void inserirVertice(String vertice) {
     // Insere o vértice sem adicionar coluna
     this.order.add(vertice);
-    ArrayList<Integer> row = new ArrayList<Integer>();
+    ArrayList<Double> row = new ArrayList<Double>();
     for (int i = 0; i < this.getVerticeQuantity()-1; i++) {
-      row.add(0);
+      row.add(0d);
     }
     this.matrix.add(row);
     // Adiciona nova coluna para Matriz de Adjacência
     for (int j = 0; j < this.getVerticeQuantity(); j++) {
-      this.matrix.get(j).add(0);
+      this.matrix.get(j).add(0d);
     }
   }
 
@@ -82,14 +82,14 @@ public class StrategyAdjMatrix implements StrategyStructure {
   }
 
   @Override
-  public void inserirAresta(String n1, String n2, int peso) {
-    if (this.verticeExists(n1) && this.verticeExists(n2)) {
-      if (this.arestaExists(n1, n2)) {
+  public void inserirAresta(String v_origem, String v_destino, double peso) {
+    if (this.verticeExists(v_origem) && this.verticeExists(v_destino)) {
+      if (this.arestaExists(v_origem, v_destino)) {
         throw new UnsupportedOperationException("Simple Graph doesn't accept more than 1 edge"); 
       }
       else {
-        int index1 = this.order.indexOf(n1);
-        int index2 = this.order.indexOf(n2);
+        int index1 = this.order.indexOf(v_origem);
+        int index2 = this.order.indexOf(v_destino);
         this.matrix.get(index1).set(index2, peso); 
       }
     } 
@@ -98,28 +98,28 @@ public class StrategyAdjMatrix implements StrategyStructure {
   @Override
   public void inserirConjuntoArestas(ArrayList<String[]> arestas) {
     for (String[] aresta : arestas) {
-      this.inserirAresta(aresta[0], aresta[1], Integer.valueOf(aresta[2]));
+      this.inserirAresta(aresta[0], aresta[1], Double.valueOf(aresta[2]));
     }
   } 
   
   @Override
-  public void removerAresta(String n1, String n2) {
-    if (this.arestaExists(n1, n2)) {
-      int index1 = this.order.indexOf(n1);
-      int index2 = this.order.indexOf(n2);
-      this.matrix.get(index1).set(index2, 0);
+  public void removerAresta(String v_origem, String v_destino) {
+    if (this.arestaExists(v_origem, v_destino)) {
+      int index1 = this.order.indexOf(v_origem);
+      int index2 = this.order.indexOf(v_destino);
+      this.matrix.get(index1).set(index2, 0d);
     }
   } 
  
   @Override
-  public boolean verticeExists(String n1) {
-    return (this.order.contains(n1))? true : false;
+  public boolean verticeExists(String vertice) {
+    return (this.order.contains(vertice))? true : false;
   } 
 
   @Override
-  public boolean arestaExists(String n1, String n2) {
-    if (this.verticeExists(n1) && this.verticeExists(n2)) {
-      return (this.getArestaValue(n1, n2) > 0)? true : false;
+  public boolean arestaExists(String v_origem, String v_destino) {
+    if (this.verticeExists(v_origem) && this.verticeExists(v_destino)) {
+      return (this.getArestaValue(v_origem, v_destino) > 0)? true : false;
     }
     return false;
   } 
@@ -132,8 +132,8 @@ public class StrategyAdjMatrix implements StrategyStructure {
   @Override
   public int getArestaQuantity() {
     int n_arestas = 0;
-    for (ArrayList<Integer> row : this.matrix) { // Percorre a coluna
-      for (int aresta : row) {
+    for (ArrayList<Double> row : this.matrix) { // Percorre a coluna
+      for (double aresta : row) {
         n_arestas += (aresta > 0)? 1 : 0;
       }
     } 
@@ -141,32 +141,32 @@ public class StrategyAdjMatrix implements StrategyStructure {
   } 
 
   @Override
-  public int getGrau(String n1) {
-    if (this.verticeExists(n1)) {
+  public int getGrau(String vertice) {
+    if (this.verticeExists(vertice)) {
       if (this.isDigrafo()) {
-        return this.getGrauEntradaD(n1) + this.getGrauSaidaD(n1);
+        return this.getGrauEntradaD(vertice) + this.getGrauSaidaD(vertice);
       }
       // Se nao for Digrafo, 
       // entao pra toda de entrada tem sua saida mapeada.
       // E entao o codigo acima repetiria aresta.
-      return this.getGrauGeralND(n1);
+      return this.getGrauGeralND(vertice);
     }   
     return -1; // Maybe should return error instead
   } 
 
   @Override
-  public int getGrauGeralND(String n1) {
-    if (this.verticeExists(n1)) {
+  public int getGrauGeralND(String vertice) {
+    if (this.verticeExists(vertice)) {
       int grau = 0;
       for (int i = 0; i < this.getVerticeQuantity(); i++) {
         String destino = this.order.get(i);
-        int ida = this.getArestaValue(n1, destino);
-        int volta = this.getArestaValue(destino, n1); 
+        double ida = this.getArestaValue(vertice, destino);
+        double volta = this.getArestaValue(destino, vertice); 
         if (ida == 0) {
           continue;
         } 
         if (ida == volta) {
-          grau += (n1.equals(destino))? 2 : 1;
+          grau += (vertice.equals(destino))? 2 : 1;
         }
       }
       return grau;
@@ -175,12 +175,12 @@ public class StrategyAdjMatrix implements StrategyStructure {
   } 
 
   @Override
-  public int getGrauEntradaD(String n1) {
-    if (this.verticeExists(n1)) {
-      int index1 = this.order.indexOf(n1);
+  public int getGrauEntradaD(String vertice) {
+    if (this.verticeExists(vertice)) {
+      int index1 = this.order.indexOf(vertice);
       int grau = 0;
   
-      for (ArrayList<Integer> col : this.matrix) { // Percorre a coluna
+      for (ArrayList<Double> col : this.matrix) { // Percorre a coluna
         grau += (col.get(index1) > 0)? 1 : 0;
       }
       return grau;
@@ -189,13 +189,13 @@ public class StrategyAdjMatrix implements StrategyStructure {
   }
 
   @Override
-  public int getGrauSaidaD(String n1) {
-    if (this.verticeExists(n1)) {
-      int index1 = this.order.indexOf(n1);
+  public int getGrauSaidaD(String vertice) {
+    if (this.verticeExists(vertice)) {
+      int index1 = this.order.indexOf(vertice);
       int grau = 0;
   
-      for (Integer row : this.matrix.get(index1)) { // Percorre a linha
-        grau += (row > 0)? 1 : 0;
+      for (Double value_on_row : this.matrix.get(index1)) { // Percorre a linha
+        grau += (value_on_row > 0)? 1 : 0;
       }
       return grau;
     } 
@@ -248,11 +248,11 @@ public class StrategyAdjMatrix implements StrategyStructure {
     }
     grafo += "\n";
 
-    for (ArrayList<Integer> row : this.matrix) {
+    for (ArrayList<Double> row : this.matrix) {
       grafo += this.order.get(i);
       grafo += " ";
-      for (Integer value : row) {
-        grafo += Integer.toString(value);
+      for (Double value : row) {
+        grafo += Double.toString(value);
         grafo += " ";
       }
       grafo += "\n";
